fece5b7e57c3ef4dc9a7dcb0b0ed016873c4bc77
KristiyanTodorof <kristiyantod10@gmail.com>
2025-04-12 23:21:39 +0300

    Initial commit

---
+++ Binary/BinarySearchExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class BinarySearchExample
+    {
+        public static int Binary(int[] array, int target)
+        {
+            int low = 0;
+            int high =  array.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (array[mid] == target)
+                {
+                    return mid;
+                }
+                    
+
+                if (target < array[mid])
+                {
+                    high = mid - 1;
+                }
+
+                else
+                {
+                    low = mid + 1;
+                }
+                    
+            }
+            return -1;
+        }
+    }
+}
---
+++ Binary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BinarySearch.BinarySearchExample;
+
+
+            int[] sortedArray = { 3, 9, 10, 27, 38, 43, 82 };
+            int target = 43;
+
+            Console.WriteLine("Sorted Array: " + string.Join(", ", sortedArray));
+            Console.WriteLine("Target Element: " + target);
+
+            int result = Binary(sortedArray, target);
+
+            if (result != -1)
+            {
+                Console.WriteLine($"Element found at index: {result}");
+            }
+
+            else
+            {
+                Console.WriteLine("Element not found in the array.");
+            }
