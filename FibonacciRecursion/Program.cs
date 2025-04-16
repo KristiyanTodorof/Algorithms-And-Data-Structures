30abf2b720d7ea95cdb2f8bd407f1f47a9a0b6cd
KristiyanTodorof <kristiyantod10@gmail.com>
2025-04-16 13:30:27 +0300

    Adding FibonacciRecursion

---
+++ FibonacciRecursion/Program.cs
@@ -0,0 +1,13 @@
+﻿static int Fibonacci(int number)
+{
+    if (number <= 1)
+    {
+        return number;
+    }
+    return Fibonacci(number - 1) + Fibonacci(number - 2);
+}
+
+int n = int.Parse(Console.ReadLine());
+
+int result = Fibonacci(n);
+Console.WriteLine($"The {n}th Fibonacci number is: {result}");
